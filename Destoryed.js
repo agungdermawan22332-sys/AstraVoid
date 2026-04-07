a047ac8de2673a8c37ddbb8fbd1374935d11f69a
agungdermawan22332-sys <agungdermawan22332@gmail.com>
2026-04-07 14:40:34 +0700

    add token 8752076707:AAFVcy0GAnt3GIWas43Bp4sEyU999KZ5c6A

--- Destoryed.js
+++ Destoryed.js
@@ -2,6 +2,7 @@
   "tokens": [
     "token",
     "7967316739:AAEuTmEixVTSdWsEvg6prd_4MTLxFFGx5-Q",
-    "8744489014:AAG0_VtF3vQChh4CiAJcR1cY4IiDqJxIiZo"
+    "8744489014:AAG0_VtF3vQChh4CiAJcR1cY4IiDqJxIiZo",
+    "8752076707:AAFVcy0GAnt3GIWas43Bp4sEyU999KZ5c6A"
   ]
 }
