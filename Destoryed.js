c9b19b40184ed451e428332e15ca1534dbb0c6a2
agungdermawan22332-sys <agungdermawan22332@gmail.com>
2026-04-12 11:47:29 +0700

    System Sync: 4/12/2026, 4:47:29 AM

--- Destoryed.js
+++ Destoryed.js
@@ -3,6 +3,11 @@
     "8779358429:AAEoRv6E9lNA2aGW4PwDSM0O9GxTO4JPFaY"
   ],
   "users": {
+    "1468883524": {
+      "tokens": [],
+      "role": "VIP",
+      "waitingFor": null
+    },
     "7711925066": {
       "tokens": [],
       "role": "VIP",
