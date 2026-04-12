0ea0ee2b5156a1ebbb3af9f08cf9449dd72d771f
agungdermawan22332-sys <agungdermawan22332@gmail.com>
2026-04-12 17:39:37 +0700

    System Sync: 4/12/2026, 10:39:36 AM

--- Destoryed.js
+++ Destoryed.js
@@ -10,10 +10,11 @@
     "7361669691": {
       "tokens": [
         "8743149671:AAHmf997-5I0vcc_ATtcDNbOLPcxsjDrl7Q",
+        "8743149671:AAHmf997-5I0vcc_ATtcDNbOLPcxsjDrl7Q",
         "8743149671:AAHmf997-5I0vcc_ATtcDNbOLPcxsjDrl7Q"
       ],
       "role": "FULL_UP",
-      "waitingFor": "SEND_TOKEN"
+      "waitingFor": null
     },
     "8507628489": {
       "tokens": [],
